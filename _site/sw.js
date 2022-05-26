987434dea51b55bf6ed9e93d7cef61362c382c36
Cyrille <91067824+cmosbeux@users.noreply.github.com>
2022-05-26 15:59:17 +0200

    dumy

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220526.1540';
+const cacheName = 'chirpy-20220526.1556';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
